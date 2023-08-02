fd9e0405a4eede3408096ae221562e04c30a8d71
Clément DELORD <cdelord@dawan.fr>
2023-08-02 16:02:58 +0200

    Cycle de vie

--- POO/index.php
+++ POO/index.php
@@ -3,8 +3,12 @@
 include('classes/Autoloader.php');
 
 $perso1 = new Guerrier("Gimly", 20, 10, 10);
-$perso2 = new Guerrier("Gandalf", 20, 10, 10);
+$perso2 = new Mage("Gandalf", 20, 10, 10);
 
 $perso1->setHp($perso1->getHp() - $perso2->getAttack());
 
-echo $perso1->getHp();
+// echo $perso1->getHp();
+
+unset($perso1);
+
+var_dump($perso1);
