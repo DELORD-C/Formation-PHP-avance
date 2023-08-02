85a25cd40afe4104a9313c1b527838fb8f29da14
Clément DELORD <cdelord@dawan.fr>
2023-08-02 10:27:56 +0200

    updated json env

--- Api/index.php
+++ Api/index.php
@@ -1,9 +1,9 @@
 <?php
 
-$env = file_get_contents("env.json");
+$env = json_decode(file_get_contents("env.json"));
 
 $response = file_get_contents('https://api.themoviedb.org/3/search/movie?api_key='
-    . $env['api_key']
+    . $env->api_key
     . '&query=barbie');
 
 $resultat = json_decode($response);
