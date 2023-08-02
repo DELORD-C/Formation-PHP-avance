bf6035865961dfdc3077b0aacda87153e0bd8a7d
Clément DELORD <cdelord@dawan.fr>
2023-08-02 09:24:45 +0200

    api

---
+++ Api/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$env = file_get_contents("env.json");
+
+$response = file_get_contents('https://api.themoviedb.org/3/search/movie?api_key='
+    . $env['api_key']
+    . '&query=barbie');
+
+$resultat = json_decode($response);
+
+var_dump($resultat);
